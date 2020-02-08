33d6ee1c2577d509184919a31f73afb1d7ace49e
onrush1995 <moniruzzaman.eng1995@gmail.com>
2020-02-08 22:32:55 +0200

    feat:Added quiz questions for the quiz application

---
+++ index.js
@@ -0,0 +1,6 @@
+// 5 question for my quiz applicaion 
+prompt("What is the capital of Finland?")
+prompt("What is the biggest lake in Finland?")
+prompt("How many countries share border with Finland?")
+prompt("What is the biggest river in Finland?")
+prompt("what is the biggest Eco park in Finland?")
