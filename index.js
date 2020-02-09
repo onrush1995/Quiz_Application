e0da0646ba7ac7c8ad751ec6c19da1647f44693d
onrush1995 <moniruzzaman.eng1995@gmail.com>
2020-02-09 23:11:52 +0200

    feat:Print all the question by taking the input

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// 5 question for my quiz applicaion 
-prompt("What is the capital of Finland?")
-prompt("What is the biggest lake in Finland?")
-prompt("How many countries share border with Finland?")
-prompt("What is the biggest river in Finland?")
-prompt("what is the biggest Eco park in Finland?")
+// First question 
+var ques1 = prompt("What is the capital of Finland?");
+document.write(ques1);
+// Second question
+var ques2 = prompt("What is the biggest lake in Finland?");
+document.write(ques2);
+// Third question
+var ques3 = prompt("How many countries share border with Finland?")
+document.write(ques3);
+// Fourth question
+var ques4 = prompt("What is the biggest river in Finland?")
+document.write(ques4);
+// Fifth question
+var ques5 = prompt("what is the biggest Eco park in Finland?")
+document.write(ques5);
