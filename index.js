c5173d19de11e7b707c30bf16c77c3bad9da3399
onrush1995 <moniruzzaman.eng1995@gmail.com>
2020-02-09 23:18:34 +0200

    fix:index file in fixex

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ document.write(ques1);
 var ques2 = prompt("What is the biggest lake in Finland?");
 document.write(ques2);
 // Third question
-var ques3 = prompt("How many countries share border with Finland?")
+var ques3 = prompt("How many countries share border with Finland?");
 document.write(ques3);
 // Fourth question
-var ques4 = prompt("What is the biggest river in Finland?")
+var ques4 = prompt("What is the biggest river in Finland?");
 document.write(ques4);
 // Fifth question
-var ques5 = prompt("what is the biggest Eco park in Finland?")
+var ques5 = prompt("what is the biggest Eco park in Finland?");
 document.write(ques5);
