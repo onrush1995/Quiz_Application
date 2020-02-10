58246bdf881ab5bda7095aaf21a2b1263581cd88
onrush1995 <moniruzzaman.eng1995@gmail.com>
2020-02-10 09:46:26 +0200

    feat:all question are validated by if else statement

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-// First question 
+// All answer
+firstQuesAns = "helsinki";
+secondQuesAns = "saimaa"
+thirdQuesAns = "3";
+fourthQuesAns = "kemijoki";
+fifthQuesAns = "lemmenjoki";
+// First question
 var ques1 = prompt("What is the capital of Finland?");
-document.write(ques1);
-// Second question
-var ques2 = prompt("What is the biggest lake in Finland?");
-document.write(ques2);
-// Third question
-var ques3 = prompt("How many countries share border with Finland?");
-document.write(ques3);
-// Fourth question
-var ques4 = prompt("What is the biggest river in Finland?");
-document.write(ques4);
-// Fifth question
-var ques5 = prompt("what is the biggest Eco park in Finland?");
-document.write(ques5);
-
-if (ques1 == "helsinki"){
+if (ques1.toLowerCase = firstQuesAns){
     document.writeln("Congratulation!!!");
 }else{
-    document.writeln("<p>"+ "Your answer " + ques1 + " is wrong.The correct answer is Helsinki"+".</p>");
+    document.writeln("<p>"+ "Your answer " + ques1 + " is wrong.The correct answer is Helsinki"+ ".</p>");
+}
+
+// Second question
+var ques2 = prompt("What is the biggest lake in Finland?");
+
+if (ques2.toLowerCase = secondQuesAns){
+    document.writeln("Congratulation!!!");
+}else{
+    document.writeln("<p>"+ "Your answer " + ques2 + " is wrong.The correct answer is "+ secondQuesAns + ".</p>");
+}
+// Third question
+var ques3 = prompt("How many countries share border with Finland?");
+if (ques3.toString = thirdQuesAns){
+    document.writeln("Congratulation!!!");
+}else{
+    document.writeln("<p>"+ "Your answer " + ques3 + " is wrong.The correct answer is"+ thirdQuesAns + ".</p>");
+}
+// Fourth question
+var ques4 = prompt("What is the biggest river in Finland?");
+if (ques4.toLowerCase = fourthQuesAns){
+    document.writeln("Congratulation!!!");
+}else{
+    document.writeln("<p>"+ "Your answer " + ques4 + " is wrong.The correct answer is "+ fourthQuesAns + ".</p>");
+}
+// Fifth question
+var ques5 = prompt("what is the biggest Eco park in Finland?");
+if (ques5.toLowerCase = fifthQuesAns){
+    document.writeln("Congratulation!!!");
+}else{
+    document.writeln("<p>"+ "Your answer " + ques5 + " is wrong.The correct answer is " + fifthQuesAns + ".</p>");
 }
