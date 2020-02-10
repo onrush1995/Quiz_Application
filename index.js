b16e81101c759a47c0b7a5533e3a1fd076910613
onrush1995 <moniruzzaman.eng1995@gmail.com>
2020-02-10 09:56:08 +0200

    fix:all the if else statement has been fixed

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ secondQuesAns = "saimaa"
 thirdQuesAns = "3";
 fourthQuesAns = "kemijoki";
 fifthQuesAns = "lemmenjoki";
+
 // First question
 var ques1 = prompt("What is the capital of Finland?");
-if (ques1.toLowerCase = firstQuesAns){
+if (ques1 == "helsinki"){
     document.writeln("Congratulation!!!");
 }else{
     document.writeln("<p>"+ "Your answer " + ques1 + " is wrong.The correct answer is Helsinki"+ ".</p>");
@@ -14,29 +15,28 @@ if (ques1.toLowerCase = firstQuesAns){
 
 // Second question
 var ques2 = prompt("What is the biggest lake in Finland?");
-
-if (ques2.toLowerCase = secondQuesAns){
+if (ques2 === "saimaa"){
     document.writeln("Congratulation!!!");
 }else{
     document.writeln("<p>"+ "Your answer " + ques2 + " is wrong.The correct answer is "+ secondQuesAns + ".</p>");
 }
 // Third question
 var ques3 = prompt("How many countries share border with Finland?");
-if (ques3.toString = thirdQuesAns){
+if (ques3 == 3){
     document.writeln("Congratulation!!!");
 }else{
-    document.writeln("<p>"+ "Your answer " + ques3 + " is wrong.The correct answer is"+ thirdQuesAns + ".</p>");
+    document.writeln("<p>"+ "Your answer " + ques3 + " is wrong.The correct answer is "+ thirdQuesAns + ".</p>");
 }
 // Fourth question
 var ques4 = prompt("What is the biggest river in Finland?");
-if (ques4.toLowerCase = fourthQuesAns){
+if (ques4 == fourthQuesAns){
     document.writeln("Congratulation!!!");
 }else{
     document.writeln("<p>"+ "Your answer " + ques4 + " is wrong.The correct answer is "+ fourthQuesAns + ".</p>");
 }
 // Fifth question
 var ques5 = prompt("what is the biggest Eco park in Finland?");
-if (ques5.toLowerCase = fifthQuesAns){
+if (ques5 == fifthQuesAns){
     document.writeln("Congratulation!!!");
 }else{
     document.writeln("<p>"+ "Your answer " + ques5 + " is wrong.The correct answer is " + fifthQuesAns + ".</p>");
