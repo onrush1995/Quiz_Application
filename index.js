6b2446e2d566f567d4fc90fa944d1aac9bcc817d
onrush1995 <moniruzzaman.eng1995@gmail.com>
2020-02-10 10:13:34 +0200

    feat:Adding score based on the correct or wrong answer

--- index.js
+++ index.js
@@ -5,39 +5,59 @@ thirdQuesAns = "3";
 fourthQuesAns = "kemijoki";
 fifthQuesAns = "lemmenjoki";
 
+// update score
+score = 0;
+
 // First question
 var ques1 = prompt("What is the capital of Finland?");
 if (ques1 == "helsinki"){
-    document.writeln("Congratulation!!!");
+    document.writeln("Congratulation!!!,you get one point");
+    if(ques1 == firstQuesAns){
+        score++;
+    }
 }else{
-    document.writeln("<p>"+ "Your answer " + ques1 + " is wrong.The correct answer is Helsinki"+ ".</p>");
+    document.writeln("<p>"+ "Your answer " + ques1 + " is wrong.The correct answer is helsinki"+ ".</p>");
 }
 
 // Second question
 var ques2 = prompt("What is the biggest lake in Finland?");
-if (ques2 === "saimaa"){
-    document.writeln("Congratulation!!!");
+if (ques2 == secondQuesAns){
+    document.writeln("Congratulation!!!,you achived one point");
+    if(ques2 == secondQuesAns){
+        score++;
+    }
 }else{
     document.writeln("<p>"+ "Your answer " + ques2 + " is wrong.The correct answer is "+ secondQuesAns + ".</p>");
 }
 // Third question
 var ques3 = prompt("How many countries share border with Finland?");
-if (ques3 == 3){
-    document.writeln("Congratulation!!!");
+if (ques3 == thirdQuesAns){
+    document.writeln("Congratulation!!!,you added one point to your score card");
+    if(ques3 == thirdQuesAns){
+        score++;
+    }
 }else{
     document.writeln("<p>"+ "Your answer " + ques3 + " is wrong.The correct answer is "+ thirdQuesAns + ".</p>");
 }
 // Fourth question
 var ques4 = prompt("What is the biggest river in Finland?");
 if (ques4 == fourthQuesAns){
-    document.writeln("Congratulation!!!");
+    document.writeln("Congratulation!!!,You have achived one point");
+    if(ques4==fourthQuesAns){
+        score++;
+    }
 }else{
     document.writeln("<p>"+ "Your answer " + ques4 + " is wrong.The correct answer is "+ fourthQuesAns + ".</p>");
 }
 // Fifth question
 var ques5 = prompt("what is the biggest Eco park in Finland?");
 if (ques5 == fifthQuesAns){
-    document.writeln("Congratulation!!!");
+    document.writeln("Congratulation!!!.You get the last question answer");
+    if(ques5 == fifthQuesAns){
+        score++;
+    }
 }else{
     document.writeln("<p>"+ "Your answer " + ques5 + " is wrong.The correct answer is " + fifthQuesAns + ".</p>");
 }
+
+document.writeln("<h3>Your socre is " + score + ".</h3>");
